afd85ec928dbe12766572e39262d9ca7b6ca4d4e
lijing <lijing@iqianjin.com>
2016-12-14 16:29:46 +0800

    runnable mybatis-spring

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 
 import com.lijing.inter.IUserOperation;
+import com.lijing.model.Article;
 import com.lijing.model.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -15,7 +16,7 @@ public class Test {
 
     static {
         try {
-            reader = Resources.getResourceAsReader("Configuration.xml");
+            reader = Resources.getResourceAsReader("conf/Configuration.xml");
             sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
         } catch (Exception e) {
             e.printStackTrace();
@@ -60,11 +61,11 @@ public class Test {
         }
     }
 
-    public void updateUser(){
+    public void updateUser() {
         //先得到用户,然后修改，提交。
         SqlSession session = sqlSessionFactory.openSession();
         try {
-            IUserOperation userOperation=session.getMapper(IUserOperation.class);
+            IUserOperation userOperation = session.getMapper(IUserOperation.class);
             User user = userOperation.selectUserByID(6);
             user.setUserAddress("原来是魔都的浦东创新园区");
             userOperation.updateUser(user);
@@ -77,14 +78,30 @@ public class Test {
 
     /**
      * 删除数据，删除一定要 commit.
+     *
      * @param id
      */
-    public void deleteUser(int id){
+    public void deleteUser(int id) {
         SqlSession session = sqlSessionFactory.openSession();
         try {
-            IUserOperation userOperation=session.getMapper(IUserOperation.class);
+            IUserOperation userOperation = session.getMapper(IUserOperation.class);
             userOperation.deleteUser(id);
             session.commit();
+        } finally {
+            session.close();
+        }
+    }
+
+    public void getUserArticles(int userid) {
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            IUserOperation userOperation = session.getMapper(IUserOperation.class);
+            List<Article> articles = userOperation.getUserArticles(userid);
+            for (Article article : articles) {
+                System.out.println(article.getTitle() + ":" + article.getContent() +
+                        ":作者是:" + article.getUser().getUserName() + ":地址:" +
+                        article.getUser().getUserAddress());
+            }
         } finally {
             session.close();
         }
@@ -95,7 +112,8 @@ public class Test {
 //        testUser.getUserList("%");
 //        testUser.addUser();
 //        testUser.updateUser();
-        testUser.deleteUser(6);
+//        testUser.deleteUser(6);
         testUser.getUserList("%");
+        testUser.getUserArticles(1);
     }
 }
